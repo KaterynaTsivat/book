34993bf9732470ebd20536c8c9e1004b5f8fadba
k.tsyvat <k.tsyvat@gmail.com>
2022-03-27 19:45:13 +0200

    Добавьте файлы проекта.

---
+++ book/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace book
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.download = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // download
+            // 
+            this.download.Location = new System.Drawing.Point(21, 321);
+            this.download.Name = "download";
+            this.download.Size = new System.Drawing.Size(95, 106);
+            this.download.TabIndex = 0;
+            this.download.Text = "download";
+            this.download.UseVisualStyleBackColor = true;
+            this.download.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(970, 450);
+            this.Controls.Add(this.download);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "book";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button download;
+    }
+}
+
